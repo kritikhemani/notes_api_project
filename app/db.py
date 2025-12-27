0d626348a92663f52b533706af849ec619003a4b
Kriti Khemani <kritikhemani04@gmail.com>
2025-12-27 11:00:47 +0530

    feat: Add Base class to db.py

--- app/db.py
+++ app/db.py
@@ -5,3 +5,7 @@ DATABASE_URL = "postgresql+asyncpg://postgres:password@localhost/practice1_db"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
+
+
+class Base(DeclarativeBase):
+    pass
