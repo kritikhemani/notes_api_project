5e5c3b9c515cef14988a89bd080ce8607463ac99
Kriti Khemani <kritikhemani04@gmail.com>
2025-12-28 12:08:57 +0530

    feat: create_note function to notes.py

--- app/notes.py
+++ app/notes.py
@@ -1,8 +1,17 @@
-from app.schema import NoteCreate
+from app.schema import NoteCreate, NoteRead, NoteUpdate
 from fastapi import Depends, APIRouter, HTTPException, status, Query
 from sqlalchemy.orm import Session
 from app.models import Note
 from app.database import get_db
 from sqlalchemy import select
+from app.deps import get_current_user
 
 router = APIRouter(prefix="/notes", tags=["notes"])
+
+@router.post("/", response_model=NoteRead)
+def create_note(payload: NoteCreate, db: Session = Depends(get_db), current_user=Depends(get_current_user)):
+    new_note = Note(title=payload.title, content=payload.content, owner_id=current_user.id)
+    db.add(new_note)
+    db.commit()
+    db.refresh(new_note)
+    return new_note
