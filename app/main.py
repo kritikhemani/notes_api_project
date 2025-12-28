16767cf722fa4b17c2af35e802b47e304b163dc3
Kriti Khemani <kritikhemani04@gmail.com>
2025-12-28 10:37:18 +0530

    feat: Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from app.database import engine, Base
+from app.auth import router as auth_router
+from app.notes import router as notes_router
 
 Base.metadata.create_all(bind=engine)
 
@@ -9,3 +11,4 @@ app = FastAPI(title="Notes API")
 @app.get("/")
 def home():
     return {"message": "Welcome to the Notes API!"}
+
