a187d1e4d2a3534491b083ecfb2120016b49f99d
Kriti Khemani <kritikhemani04@gmail.com>
2025-12-28 11:30:48 +0530

    feat: Add hash_password to security.py

--- app/security.py
+++ app/security.py
@@ -1,3 +1,13 @@
 from datetime import datetime, timedelta
 from jose import jwt 
 from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+SECRET_KEY = "CHANGE_ME"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+REFRESH_EXPIRES_DAYS = 7
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
