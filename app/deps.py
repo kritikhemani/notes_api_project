89eefd2335c6b3765f488b45852401dfd2fbd5a5
Kriti Khemani <kritikhemani04@gmail.com>
2025-12-28 11:48:04 +0530

    feat: Update get_current_user to deps.py

--- app/deps.py
+++ app/deps.py
@@ -3,10 +3,20 @@ from sqlalchemy.orm import Session
 from app.database import get_db, SessionLocal
 from app.models import User
 from fastapi.security import OAuth2PasswordBearer
+from app.security import decode_token
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
-    pass
+    try:
+        payload = decode_token(token)
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Token")
+    if payload.get("type") != "access":
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Token Type")
+    user = db.query(User).get(int(payload["sub"]))
+    if not user or not user.is_active:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Inactive User")
+    return user
     
