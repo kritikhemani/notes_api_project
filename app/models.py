f0dfae3bd554cc333643b69e9fc6c395a73007af
Kriti Khemani <kritikhemani04@gmail.com>
2025-12-28 10:53:00 +0530

    feat: Update User class to models.py

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     name: Mapped[str] = mapped_column(index=True)
     email: Mapped[str] = mapped_column(unique=True, index=True, nullable=False)
-    hashed_password: Mapped[str] = mapped_column(nullable_=False)
+    hashed_password: Mapped[str] = mapped_column()
     is_active: Mapped[bool] = mapped_column(default=True)
 
 class Note(Base):
