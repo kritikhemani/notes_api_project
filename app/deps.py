78a9012f4fceaa8b1901a82439bc8b81eb94a120
Kriti Khemani <kritikhemani04@gmail.com>
2025-12-28 11:17:40 +0530

    feat: Add get_current_user() function to deps.py

--- app/deps.py
+++ app/deps.py
@@ -1,4 +1,12 @@
 from fastapi import Depends, HTTPException, status
 from sqlalchemy.orm import Session
-from app.database import get_db
+from app.database import get_db, SessionLocal
 from app.models import User
+from fastapi.security import OAuth2PasswordBearer
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    pass
+    
