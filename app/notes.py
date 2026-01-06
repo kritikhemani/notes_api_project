114779c3367b7c98bd8a647e28ecb948af811d0e
Kriti Khemani <kritikhemani04@gmail.com>
2026-01-06 11:50:52 +0530

    feat: Update notes.py

--- app/notes.py
+++ app/notes.py
@@ -3,7 +3,6 @@ from fastapi import Depends, APIRouter, HTTPException, status, Query
 from sqlalchemy.orm import Session
 from app.models import Note
 from app.database import get_db
-from sqlalchemy import select
 from app.deps import get_current_user
 
 router = APIRouter(prefix="/notes", tags=["notes"])
