1f514a3a7d0de9626b861cb3baa1f2d003e76864
Kriti Khemani <kritikhemani04@gmail.com>
2026-01-06 12:22:38 +0530

    feat: Add async to read_notes function in notes.py

--- app/notes.py
+++ app/notes.py
@@ -16,7 +16,7 @@ async def create_note(payload: NoteCreate, db: Session = Depends(get_db), curren
     return new_note
 
 @router.get("/read/")
-def read_notes(skip: int = Query(0, ge=0), limit: int = Query(10, le=100), db: Session = Depends(get_db), current_user=Depends(get_current_user)):
+async def read_notes(skip: int = Query(0, ge=0), limit: int = Query(10, le=100), db: Session = Depends(get_db), current_user=Depends(get_current_user)):
     notes = db.query(Note).filter(Note.owner_id == current_user.id).offset(skip).limit(limit).all()
     return notes
 
