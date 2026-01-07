3d5c9789fe0f89ba265a4b4a7dc1ef84d8bccec5
Kriti Khemani <kritikhemani04@gmail.com>
2026-01-07 10:39:15 +0530

    feat: Update main.py

--- app/main.py
+++ app/main.py
@@ -3,9 +3,9 @@ from app.database import engine, Base
 from app.auth import router as auth_router
 from app.notes import router as notes_router
 
-Base.metadata.create_all(bind=engine)
-
 app = FastAPI(title="Notes API")
+
+
 
 
 @app.get("/")
