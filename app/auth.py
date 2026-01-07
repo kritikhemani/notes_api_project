fd44bf0c0b15cc834d8bfb5b8ebff8cdda0c9bd6
Kriti Khemani <kritikhemani04@gmail.com>
2026-01-07 10:54:58 +0530

    feat: Update register function in auth.py

--- app/auth.py
+++ app/auth.py
@@ -5,6 +5,7 @@ from app.models import User
 from app.database import get_db
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
+from sqlalchemy.future import select
 from app.security import hash_password, create_access_token, create_refresh_token, decode_token, verify_password
 
 
@@ -12,13 +13,14 @@ router = APIRouter(prefix="/auth", tags=["auth"])
 
 @router.post("/register", response_model=UserRead)
 async def register(payload: UserCreate, db: AsyncSession = Depends(get_db)):
-    existing_user = db.query(User).filter(User.email == payload.email).first()
+    result = await db.execute(select(User).where(User.email == payload.email))
+    existing_user = result.scalar_one_or_none()
     if existing_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     new_user = User(name=payload.name, email=payload.email, hashed_password=hash_password(payload.password))
     db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
+    await db.commit()
+    await db.refresh(new_user)
     return new_user
 
 @router.post("/login", response_model=Token)
