ed4779721790f86e955e51cc0ec4902f53203142
Kriti Khemani <kritikhemani04@gmail.com>
2026-01-09 10:50:04 +0530

    feat: Update update_notes function in notes.py

--- app/notes.py
+++ app/notes.py
@@ -34,8 +34,8 @@ async def update_notes(note_id: int, payload: NoteUpdate, db: AsyncSession = Dep
         note.title = payload.title
         if note.content is not None:
             note.content = payload.content
-    db.commit()
-    db.refresh(note)
+    await db.commit()
+    await db.refresh(note)
     return note
 
 @router.delete("/delete/{note_id}/")
