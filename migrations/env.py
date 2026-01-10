1b75286f3f16daef86d15ba7c59763662c164e7b
Kriti Khemani <kritikhemani04@gmail.com>
2026-01-10 10:14:11 +0530

    Import Base from app.database to migrations/env.py

--- migrations/env.py
+++ migrations/env.py
@@ -8,6 +8,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 
 from app import models
+from app.database import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
