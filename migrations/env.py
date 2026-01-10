bb342012ad334e02f98fdd0b93109e8d08134fa8
Kriti Khemani <kritikhemani04@gmail.com>
2026-01-10 10:12:59 +0530

    feat: Import models from app to migrations/env.py

--- migrations/env.py
+++ migrations/env.py
@@ -7,6 +7,8 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
+from app import models
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
