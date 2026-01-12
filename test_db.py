51811d226a995d238a8ed6228085a5e4f8e667de
Kriti Khemani <kritikhemani04@gmail.com>
2026-01-12 10:33:07 +0530

    feat: Import COnfig from alembic.config to test_db.py

--- test_db.py
+++ test_db.py
@@ -1,6 +1,7 @@
 import asyncio
 from sqlalchemy.ext.asyncio import create_async_engine
 from alembic import command
+from alembic.config import Config
 
 TEST_DB_URL = "postgresql+asyncpg://postgres:password@localhost/project_test_db"
 
