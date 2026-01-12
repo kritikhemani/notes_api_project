d1914e9799c3e94a9bc7ed4a28781d49358622bf
Kriti Khemani <kritikhemani04@gmail.com>
2026-01-12 10:36:04 +0530

    feat: Making some changes to create_test_db function in test_db.py

--- test_db.py
+++ test_db.py
@@ -6,4 +6,5 @@ from alembic.config import Config
 TEST_DB_URL = "postgresql+asyncpg://postgres:password@localhost/project_test_db"
 
 def create_test_db():
-    pass
+    engine = create_async_engine(TEST_DB_URL)
+    
