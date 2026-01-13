ad112732334d2045534a73ed7cc3d20895e35774
Kriti Khemani <kritikhemani04@gmail.com>
2026-01-13 10:26:11 +0530

    Making some changes to test_db.py

--- test_db.py
+++ test_db.py
@@ -4,12 +4,13 @@ from alembic.config import Config
 
 TEST_DB_URL = "postgresql+asyncpg://postgres:password@localhost/project_test_db"
 
+alembic_cfg = Config("alembic.ini")
+alembic_cfg.set_main_option("sqlalchemy.url", TEST_DB_URL)
+
 def create_test_db():
     engine = create_async_engine(TEST_DB_URL)
     asyncio.run(engine.run_sync(Base.metadata.create_all))
     
-alembic_cfg = Config("alembic.ini")
-alembic_cfg.set_main_option("sqlalchemy.url", TEST_DB_URL)
     
 def run_migrations():
     command.upgrade(alembic_cfg, "head")
