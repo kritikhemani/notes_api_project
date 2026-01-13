f7bba6aa6ba51774baa9015b2c0d0f68e3863ee6
Kriti Khemani <kritikhemani04@gmail.com>
2026-01-13 10:48:07 +0530

    feat: Adding async function test_register_and_create_note

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,3 +8,7 @@ from app.main import app
 async def client():
     async with AsyncClient(app=app, base_url="http://testserver") as async_client:
         yield async_client
+        
+@pytest.mark.asyncio
+async def test_register_and_create_note(client):
+    pass
