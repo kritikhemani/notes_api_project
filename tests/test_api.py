6e9e8af3b840d0e48eaffff29dbd546aa424bd77
Kriti Khemani <kritikhemani04@gmail.com>
2026-01-13 10:40:47 +0530

    feat: Making some changes to test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,13 +1,8 @@
 import pytest
-from test_db import TEST_DB_URL
 import pytest_asyncio
 from httpx import AsyncClient
 from app.main import app
-from alembic.config import Config
-from alembic import command
 
-alembic_cfg = Config("alembic.ini")
-alembic_cfg.set_main_option("sqlalchemy.url", TEST_DB_URL)
 
     
 @pytest_asyncio.fixture
