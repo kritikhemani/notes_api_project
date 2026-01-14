5ab14b9940273f31ec94e2faa19bd42f1e084469
Kriti Khemani <kritikhemani04@gmail.com>
2026-01-14 10:44:38 +0530

    feat: Making some changes to tests/test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,10 +4,7 @@ from httpx import AsyncClient
 from app.main import app
 
   
-@pytest_asyncio.fixture
-async def client():
-    async with AsyncClient(app=app, base_url="http://testserver") as async_client:
-        yield async_client
+
         
 @pytest.mark.asyncio
 async def test_register_and_create_note(client):
