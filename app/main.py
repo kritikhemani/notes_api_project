8e92c8853febb27f7cc9ee2feb6ed2e983cc9883
Kriti Khemani <kritikhemani04@gmail.com>
2026-01-17 10:05:55 +0530

    feat: Update lifespan function to main.py

--- app/main.py
+++ app/main.py
@@ -8,6 +8,9 @@ from app.notes import router as notes_router
 async def lifespan(app: FastAPI):
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+    yield
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
 
 
 app = FastAPI(title="Notes API")
