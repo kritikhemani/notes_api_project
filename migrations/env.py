4c2f2dd9606345b29260d0878fd9e224d4ecf4c4
Kriti Khemani <kritikhemani04@gmail.com>
2026-01-22 10:39:46 +0530

    feat: Making some changes to migrations/env.py

--- migrations/env.py
+++ migrations/env.py
@@ -83,7 +83,7 @@ async def run_async_migrations() -> None:
 def run_migrations_online() -> None:
     """Run migrations in 'online' mode."""
 
-    asyncio.run(run_async_migrations())
+    
 
 
 if context.is_offline_mode():
