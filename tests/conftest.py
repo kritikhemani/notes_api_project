7e59243361e704642d5132a4b18fb5c37f07982a
Kriti Khemani <kritikhemani04@gmail.com>
2026-01-24 19:37:58 +0530

    feat: Delete the event_loop fixture function from tests/conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,12 +17,6 @@ TEST_DB_URL = "postgresql+asyncpg://postgres:password@localhost/project_test_db"
 
 AsyncSessionLocal = async_sessionmaker(bind=create_async_engine(TEST_DB_URL, poolclass=NullPool), class_=AsyncSession, expire_on_commit=False)
 
-@pytest.fixture(scope="session")
-def event_loop():
-    policy = asyncio.get_event_loop_policy()
-    loop = policy.new_event_loop()
-    yield loop
-    loop.close()
     
 @pytest.fixture(scope="session")
 def test_engine():
