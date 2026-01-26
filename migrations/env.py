ffde6788e95afbf66be59748d67bf9ca0a411230
Kriti Khemani <kritikhemani04@gmail.com>
2026-01-26 13:29:06 +0530

    feat: Making some changes to migrations/env.py

--- migrations/env.py
+++ migrations/env.py
@@ -94,9 +94,9 @@ async def run_migrations_online() -> None:
         poolclass=pool.NullPool,
     )
         
-    async with connectable.connect() as connection:
-        await connection.run_sync(do_run_migrations)
-    await connectable.dispose()
+    with connectable.connect() as connection:
+        connection.run_sync(do_run_migrations)
+    connectable.dispose()
 
 
 if context.is_offline_mode():
