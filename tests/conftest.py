0004927292160c9df2ea58a926c85749ed16295b
Kriti Khemani <kritikhemani04@gmail.com>
2026-01-28 11:14:06 +0530

    feat: Update override_get_db fixture function in tests/conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,7 +23,7 @@ async def db_session():
         await session.rollback()
         
 @pytest.fixture(autouse=True)
-async def override_get_db(db_session):
+async def override_get_db(db_session: AsyncSession):
     async def _override():
         yield db_session
     app.dependency_overrides[get_db] = _override
