79d7238f24cdf3f2ba6efa0870a4fbfec122c6f3
Kriti Khemani <kritikhemani04@gmail.com>
2026-02-07 12:56:29 +0530

    feat: Update app/middleware.py

--- app/middleware.py
+++ app/middleware.py
@@ -2,3 +2,5 @@ from slowapi import Limiter
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 from slowapi.middleware import SlowAPIMiddleware
+
+limiter = Limiter(key_func=get_remote_address)
