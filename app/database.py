948fe8e9f27ed23f506b89fbf8a48e17d3a60ab9
Kriti Khemani <kritikhemani04@gmail.com>
2026-03-10 13:07:16 +0530

    feat: Making some changes to app/database.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from typing import AsyncGenerator
 
-DATABASE_URL = "postgresql+asyncpg://postgres:password@localhost/project_db"
+
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession, expire_on_commit=False)
