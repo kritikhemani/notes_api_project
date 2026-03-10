64748138a1951ddca935d9f9a74e2577ff484344
Kriti Khemani <kritikhemani04@gmail.com>
2026-03-10 13:11:40 +0530

    feat: Set the database_url in database.py

--- app/database.py
+++ app/database.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession, expire_on_commit=False)
 
