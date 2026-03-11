08f58a37e0715824b7555b843da083abdc3cad25
Kriti Khemani <kritikhemani04@gmail.com>
2026-03-11 12:25:30 +0530

    feat: Update app.main.py

--- app/main.py
+++ app/main.py
@@ -23,5 +23,5 @@ app.include_router(auth_router)
 app.include_router(notes_router)
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000)
 
