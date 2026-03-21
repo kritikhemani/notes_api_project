9ccfe7d615112278feb44603f7ae43dcee4a03b1
Kriti Khemani <kritikhemani04@gmail.com>
2026-03-21 11:44:53 +0530

    Making some changes to main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from app.database import engine, Base
 from app.auth import router as auth_router
 from app.notes import router as notes_router
-import uvicorn
 from app.middleware import limiter
 from slowapi.errors import RateLimitExceeded
 from slowapi.middleware import SlowAPIMiddleware
@@ -26,6 +25,4 @@ async def home():
 app.include_router(auth_router)
 app.include_router(notes_router)
 
-if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000)
 
